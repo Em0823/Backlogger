4e1172eae3c23aae8a8ee7a793df208cef8e1301
Fernando Collante <fernandocollante@live.com>
2024-03-30 15:21:27 -0400

    removed dup cat selector, added modal

--- script.js
+++ script.js
@@ -42,49 +42,6 @@ document.querySelector('#create-backlog-form').addEventListener('submit', functi
     }
 });
 
-// Function to remove all selected states and then apply to the clicked tab
-function selectTab(button) {
-    categoryButtons.forEach(btn => btn.classList.remove('selected-tab'));
-    button.classList.add('selected-tab');
-}
-
-// Function to show or hide media cards based on category
-function toggleMediaCardsByCategory(category, isActive) {
-    if (isActive) {
-        const mediaCardsToShow = document.querySelectorAll(`.media-card[data-category="${category}"]`);
-        mediaCardsToShow.forEach(card => {
-            card.style.display = 'flex';
-        });
-    } else {
-        // Show all media cards if the filter is deactivated
-        const mediaCards = document.querySelectorAll('.media-card');
-        mediaCards.forEach(card => {
-            card.style.display = 'flex';
-        });
-    }
-}
-
-// Event listeners for category buttons
-const categoryButtons = document.querySelectorAll('.category-selector button');
-
-categoryButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        const isActive = this.classList.contains('selected-tab');
-        // Remove the selected state from all tabs
-        categoryButtons.forEach(btn => btn.classList.remove('selected-tab'));
-
-        // If the clicked button was already active, show all media cards
-        // Otherwise, apply the filter
-        if (!isActive) {
-            this.classList.add('selected-tab');
-            const category = this.textContent.trim().toLowerCase().replace(/\s+/g, '-');
-            toggleMediaCardsByCategory(category, true);
-        } else {
-            // If no tab is selected, show all media cards
-            toggleMediaCardsByCategory('', false);
-        }
-    });
-});
 
 // Function to update dropdown text
 function updateDropdownText(text) {
@@ -126,48 +83,130 @@ window.addEventListener('click', function(event) {
 
 document.addEventListener("DOMContentLoaded", function() {
     const filterIcons = document.querySelectorAll('.nav-links i');
+    let activeFilter = null; // Keep track of the active filter
 
     filterIcons.forEach(icon => {
       icon.addEventListener('click', function() {
         const type = this.getAttribute('data-type');
-        filterList(type);
+        
+        // Check if the clicked icon is already active
+        if (activeFilter === type) {
+          // If yes, show all cards and clear the active filter
+          showAllMediaCards();
+          activeFilter = null; // Reset active filter
+        } else {
+          // Otherwise, apply the filter
+          filterList(type);
+          activeFilter = type; // Update active filter to current type
+        }
       });
     });
 
     function filterList(type) {
       const allCards = document.querySelectorAll('.media-card');
-      
       allCards.forEach(card => {
         card.style.display = 'none'; // Hide all cards first
       });
-      
       const filteredCards = document.querySelectorAll(`.media-card[data-type="${type}"]`);
       filteredCards.forEach(card => {
         card.style.display = 'block'; // Show cards with the selected data-type
       });
     }
-  });
+
+    function showAllMediaCards() {
+      const allCards = document.querySelectorAll('.media-card');
+      allCards.forEach(card => {
+        card.style.display = 'block'; // Show all cards
+      });
+    }
+});
 
 
   //*-----------Menu expand toggle-------------
+
   document.addEventListener("DOMContentLoaded", function() {
-const menuExpandButton = document.querySelector('.menu-expand button');
-const menuOptions = document.querySelector('.menu-options');
+    const menuExpandButton = document.querySelector('.menu-expand button');
+    const menuOptions = document.querySelector('.menu-options');
+  
+    // This function toggles the display of the menu
+    // It's triggered when the menu button is clicked
+    function toggleMenu() {
+      const isMenuVisible = menuOptions.style.display === 'block';
+      menuOptions.style.display = isMenuVisible ? 'none' : 'block';
+    }
+  
+    // Attach the click event listener to the menu button
+    menuExpandButton.addEventListener('click', function(event) {
+      event.stopPropagation(); // Prevent the click event from propagating to the document
+      toggleMenu();
+    });
+  
+    // Hide the menu when clicking outside of the menu
+    document.addEventListener('click', function(event) {
+      if (!menuOptions.contains(event.target) && event.target !== menuExpandButton) {
+        menuOptions.style.display = 'none';
+      }
+    });
+  
+    // Hide menu options when the mouse leaves the menu area
+    // We listen for the mouseleave event on the menuOptions element itself
+    menuOptions.addEventListener('mouseleave', function() {
+      menuOptions.style.display = 'none';
+    });
+  });
 
-// Toggle menu options display when menu button is clicked
-menuExpandButton.addEventListener('click', function(event) {
-event.stopPropagation(); // Prevent the click event from propagating to the document
-toggleMenu();
-});
 
-// Hide menu options when clicking outside the menu
-document.addEventListener('click', function(event) {
-if (!menuOptions.contains(event.target) && event.target !== menuExpandButton) {
-  menuOptions.style.display = 'none';
+  // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btns = document.querySelectorAll('.media-card');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the media card, open the modal 
+btns.forEach(function(btn){
+    btn.onclick = function() {
+        modal.style.display = "block";
+        // Fill modal with content based on the clicked card
+        // e.g., modal.querySelector('.modal-content p').textContent = this.querySelector('.media-title').textContent;
+    }
+})
+
+var modalImage = document.getElementById("modalImage");
+var modalTitle = document.getElementById("modalTitle");
+var modalDetails = document.getElementById("modalDetails");
+
+// Function to fill the modal with content from the clicked card
+function fillModal(card) {
+    // Assuming the structure of your media card, adjust the selectors as needed
+    var image = card.querySelector('img').src;
+    var title = card.querySelector('.media-title').textContent;
+    var details = card.querySelector('.media-details').textContent;
+    
+    // Set the content in the modal
+    modalImage.src = image;
+    modalTitle.textContent = title;
+    modalDetails.textContent = details;
 }
-});
 
-function toggleMenu() {
-menuOptions.style.display = (menuOptions.style.display === 'block') ? 'none' : 'block';
+// Attach a click event listener to each card
+btns.forEach(function(btn){
+    btn.onclick = function() {
+        fillModal(this);
+        modal.style.display = "block";
+    }
+})
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
 }
-});
+
+// When the user clicks anywhere outside of the modal, close it
+//window.onclick = function(event) {
+//    if (event.target == modal) {
+//        modal.style.display = "none";
+//    }
+//}
